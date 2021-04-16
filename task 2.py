ac270f3cfef670cef5e370d5288c67215d4849b5
Clearymac <72894742+Clearymac@users.noreply.github.com>
2021-04-16 15:01:16 +1200

    bruh

--- task 2.py
+++ task 2.py
@@ -5,17 +5,26 @@ list = ['Evi', 'Madeleine', 'Cool guy', 'Kelsey', 'Cayden', 'Hayley', 'Darian']
 list.sort()
 print('Sorted list:', list)
 
+#asks the user to input their name
 name = input("What is your name? ")
 name = name.title()
-print(name)
 
+
+#checks if name is in list and gives option to add to list
 if name in list:
     print("Oh senpai! that name is already in the list asdahasdhahkshldgakshd uwu")
+    
+bruh = input("would you like to add your name or replace your name in the list or nothing? ")
+bruh = bruh.lower()
+
+if bruh == "add":
+    list.append(name)
+    print(list)
+elif bruh == "replace":
+    replace_name = input("What of the following names would you like to replace?\n{}\n".format(list))
+    list = list.replace(replace_name, name)
+    print(list)
+elif bruh == "nothing":
+    print("poopoo stinker")
 else:
-    replace = input("would you like to add your name to the list? ")
-    replace.lower()
-    if replace == "no":
-        print("OK stinker")
-    elif replace == "yes":
-        list.append(name)
-        print(list)
+    print("what are you on?")
