af09b7a7757cbf89a8001d4179c9721e9cc7dc6f
Clearymac <clearyd@stu.otc.school.nz>
2021-04-16 13:20:34 +1200

    better?????

--- task 2.py
+++ task 2.py
@@ -1,6 +1,19 @@
 #LIST RETARRD
 list = ['Evi', 'Madeleine', 'Cool guy', 'Kelsey', 'Cayden', 'Hayley', 'Darian']
 
+#sorts and prints the list
 list.sort()
-
 print('Sorted list:', list)
+
+name = input("What is your name? ")
+
+if name in list:
+    print("Oh senpai! that name is already in the list asdahasdhahkshldgakshd uwu")
+else:
+    replace = input("would you like to add your name to the list? ")
+    replace.lower()
+    if replace == "no":
+        print("OK stinker")
+    elif replace == "yes":
+        list += name
+        print(list)
